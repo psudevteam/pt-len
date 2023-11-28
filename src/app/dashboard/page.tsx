498ddb7a7167774e27ca2b68f94f6f0dd6010737
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-11-28 08:59:32 +0700

    fix: build time

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,25 @@
+import { prisma } from "@/libs";
 import { DashboardModule } from "@/modules";
 import { NextPage } from "next";
-import { ReactElement } from "react";
 
-const DashboardPage: NextPage = (): ReactElement => {
-  return <DashboardModule />;
+const DashboardPage: NextPage = async () => {
+  const resComplete = await prisma.reservation.count({
+    where: {
+      isCompleted: true,
+    },
+  });
+
+  const resIncomplete = await prisma.reservation.count({
+    where: {
+      isCompleted: false,
+    },
+  });
+
+  const data = {
+    approve: resComplete,
+    pending: resIncomplete,
+  };
+  return <DashboardModule data={data} />;
 };
 
 export default DashboardPage;
--- src/modules/dashboard/module.tsx
+++ src/modules/dashboard/module.tsx
@@ -1,9 +1,23 @@
 import { FC, ReactElement } from "react";
 
-export const DashboardModule: FC = (): ReactElement => {
+type TProps = {
+  data: {
+    approve: number;
+    pending: number;
+  };
+};
+
+export const DashboardModule: FC<TProps> = (props): ReactElement => {
   return (
-    <div className="flex">
-      <h1>Waduh</h1>
+    <div className="flex flex-row gap-x-4 w-full">
+      <div className="flex flex-col p-4 border shadow-md w-[200px] rounded-lg h-[200px]">
+        <span className="font-medium text-2xl">Data Selesai </span>
+        <span className="font-bold text-4xl">{props.data.approve}</span>
+      </div>
+      <div className="flex flex-col p-4 border shadow-md w-[200px] rounded-lg h-[200px]">
+        <span className="font-medium text-2xl">Data Pending</span>
+        <span className="font-bold text-4xl">{props.data.pending}</span>
+      </div>
     </div>
   );
 };
